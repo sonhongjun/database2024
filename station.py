a387fc4ee8d1a6e8a0ae5b3432cf1a31be5b7e70
sonhongjun <thsghdwns11@naver.com>
2024-12-14 04:38:35 +0900

    선택한 역에 대한 정보를 DB에서 꺼내오는 함수 추가

--- station.py
+++ station.py
@@ -10,9 +10,19 @@ def showSubwaystation():
         db = sqlite3.connect('subway_station.db')
         cursor = db.cursor()
         items = cursor.execute('SELECT distinct 역명 from 편의시설 where 역명 like (?)', ('%' + request.form['name'] + '%',)).fetchall()
-        db.commit()
         db.close()
     return render_template('search_station.html', stations = items)
+
+@app.route('/stations/<string:station_name>/', methods=['GET', 'POST'])
+def showInformation(station_name):
+    items = []
+    if request.method == 'POST':
+        db = sqlite3.connect('subway_station.db')
+        cursor = db.cursor()
+        category = request.form['category']
+        items = cursor.execute(f'SELECT * FROM {category} WHERE 역명 LIKE (?)', (station_name + '%',)).fetchall()
+        db.close()
+    return render_template('information_station.html', informations = items)
 
 if __name__ == '__main__':
     app.debug = True
